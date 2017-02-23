7e3813794ff4dae170dbcf923a3fdb9d690bcd81
ant.pu <puhongwu@live.com>
2017-02-23 22:43:04 +0800

    reg panel

--- app/build/public/js/sign.js
+++ app/build/public/js/sign.js
@@ -1,5 +1,10 @@
 "use strict";
 
+function getVarifyImgUrl() {
+    var varify_img_url = "https://account.toursforfun.com/captcha";
+    return varify_img_url + "?_t=" + new Date().getTime();
+}
+
 // 注册页面
 new Vue({
     el: "#signUp",
@@ -7,7 +12,7 @@ new Vue({
         title: "注册",
         country: "中国",
         country_code: "+86",
-        varify_status: true, // 0 不可用，1 可点击
+        varify_status: false, // 0 不可用，1 可点击
         varify_msg: "获取验证码",
         form: {
             phone: "",
@@ -15,7 +20,8 @@ new Vue({
             pwd: "",
         },
         input_active: "",
-        show_pwd: true
+        show_pwd: true,
+        varify_img: getVarifyImgUrl()
     },
     methods: {
         // 发送验证码
@@ -25,6 +31,8 @@ new Vue({
         },
         // 输入框输入
         inputVal: function(type) {
+            this.form[type] = this.form[type].trim();
+
             switch(type) {
                 case "phone":
                     if(this.form.phone.length > 6){
@@ -34,13 +42,14 @@ new Vue({
                     }
                 break;
             }
-            if(this.form[type].length > 0) {
+            
+            if(this.form[type]) {
                 this.input_active = type; 
             }
         },
         // 输入框获取焦点
         activeInput: function(type) {
-            if(this.form[type].length === 0) {
+            if(!this.form[type].trim()) {
                 this.input_active = ""; 
             }else{
                 this.input_active = type
@@ -54,9 +63,18 @@ new Vue({
         // 密码框类型转换
         showPwd: function() {
             this.show_pwd = !this.show_pwd;
+        },
+        changeVarifyImg: function() {
+            this.varify_img = getVarifyImgUrl()
         }
     },
     computed: {
-        
+        validation: function() {
+            return {
+                phone: !!this.form.phone.trim(),
+                msg_code: !!this.form.msg_code.trim(),
+                pwd: !!this.form.pwd.trim()
+            }
+        }
     }
 });
