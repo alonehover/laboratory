ad88e6f02460a32ac9491d61011051d792497660
ant.pu <puhongwu@live.com>
2017-09-30 11:29:57 +0800

    rename

--- dashboard/server/app.js
+++ dashboard/server/app.js
@@ -5,6 +5,9 @@ const router = require('./router')
 const path = require('path')
 const config = require('./config')
 
+const webpackMiddleware = require("webpack-dev-middleware")
+const webpackConfig = require("../webpack.dev.config")
+
 const app = express()
 
 router(app)
@@ -12,6 +15,8 @@ router(app)
 app.use(express.static(path.join(__dirname, '..', 'app/public')))
 app.set('views', path.join(__dirname, '..', 'app/view')); // specify the views directory
 app.set('view engine', 'ejs'); // register the template engine
+
+app.use(webpackMiddleware(webpackConfig))
 
 app.listen(config.port, function () {
     console.log('Example app listening on port 3000!')
