94708902794af39b30da720b86d936c2675d27a9
alonehover <alonehover@qq.com>
2017-11-26 17:14:13 +0800

    play

---
+++ server/controller/play.ctl.js
@@ -0,0 +1,18 @@
+const Link = require('../model/link.model')
+const Tool = require('../tool')
+
+const { Response } = Tool
+
+const Play = {
+    init(app) {
+        app.get('/play', this.show)
+    },
+
+    async show(req, res, next) {
+        res.render('play', {
+            title: "Play"
+        })
+    }
+}
+
+module.exports = Play
