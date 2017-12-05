47074b9087255a90fa4d0f84da5b7a5cd9677fc6
ant.pu <puhongwu@live.com>
2017-12-05 17:05:43 +0800

    resize

--- app/public/js/play.js
+++ app/public/js/play.js
@@ -69,7 +69,6 @@
     }
     window['Runner'] = Runner;
 
-
     /**
      * Default game width.
      * @const
@@ -119,7 +118,7 @@
         MAX_OBSTACLE_DUPLICATION: 2,
         MAX_SPEED: 13,
         MIN_JUMP_HEIGHT: 35,
-        MOBILE_SPEED_COEFFICIENT: 1.2,
+        MOBILE_SPEED_COEFFICIENT: 1.1,
         // RESOURCE_TEMPLATE_ID: 'audio-resources',
         SPEED: 6,
         SPEED_DROP_COEFFICIENT: 3
@@ -160,16 +159,21 @@
      */
     Runner.spriteDefinition = {
         LDPI: {
-            MARK_LARGE_M: { x: 332, y: 2 },
-            MARK_S_TREE: { x: 228, y: 2 },
-            CLOUD: { x: 86, y: 2 },
-            HORIZON: { x: 0, y: 60 },
-            MOON: { x: 484, y: 2 },
-            AIRPLANE: { x: 134, y: 2 },
+            MARK_LARGE_STONE: { x: 266, y: 5 }, // 大障碍物
+            MARK_LARGE_M: { x: 288, y: 5 }, // 大障碍物
+            MARK_LARGE_BUILDING: { x: 354, y: 5 }, // 大障碍物
+            MARK_S_TREE: { x: 223, y: 5 }, // 小障碍物
+            MARK_S_TREE2: { x: 223, y: 5 }, // 小障碍物
+            MARK_S_BRIDGE: { x: 115, y: 5 }, // 小障碍物
+            MARK_S_PAVILION: { x: 185, y: 5 }, // 小障碍物
+            CLOUD: { x: 4, y: 5 },    // 云
+            HORIZON: { x: 0, y: 69 },  // 地面
+            // MOON: { x: 954, y: 10 },     
+            AIRPLANE: { x: 59, y: 5 },  // 飞机
             // RESTART: { x: 2, y: 2 },
-            TEXT_SPRITE: { x: 655, y: 2 },
-            TREX: { x: 716, y: 5 },
-            STAR: { x: 645, y: 2 }
+            TEXT_SPRITE: { x: 431, y: 5 },
+            TREX: { x: 681, y: 5 }, // 人物的开始坐标
+            STAR: { x: 638, y: 5 }
         },
         HDPI: {
             MARK_LARGE_STONE: { x: 532, y: 10 }, // 大障碍物
@@ -181,7 +185,7 @@
             MARK_S_PAVILION: { x: 370, y: 10 }, // 小障碍物
             CLOUD: { x: 8, y: 10 },    // 云
             HORIZON: { x: 0, y: 138 },  // 地面
-            MOON: { x: 954, y: 10 },     
+            // MOON: { x: 954, y: 10 },     
             AIRPLANE: { x: 118, y: 10 },  // 飞机
             // RESTART: { x: 2, y: 2 },
             TEXT_SPRITE: { x: 862, y: 10 },
@@ -261,6 +265,9 @@
         //     }.bind(this));
         // },
 
+        isMobile: function() {
+            return IS_MOBILE;
+        },
         /**
          * Setting individual settings for debugging.
          * @param {string} setting
@@ -437,13 +444,14 @@
             var padding = Number(boxStyles.paddingLeft.substr(0,
                 boxStyles.paddingLeft.length - 2));
 
-            this.dimensions.WIDTH = this.outerContainerEl.offsetWidth - padding * 2;
+            var clientWIDTH = this.outerContainerEl.offsetWidth - padding * 2;
             // if (this.isArcadeMode()) {
             //     this.dimensions.WIDTH = Math.min(DEFAULT_WIDTH, this.dimensions.WIDTH);
             //     if (this.activated) {
             //         this.setArcadeModeContainerScale();
             //     }
             // }
+            this.dimensions.WIDTH = Math.min(DEFAULT_WIDTH, clientWIDTH);
 
             // Redraw the elements back onto the canvas.
             if (this.canvas) {
@@ -2373,155 +2381,155 @@
      * Nightmode shows a moon and stars on the horizon.
      * 夜晚
      */
-    function NightMode(canvas, spritePos, containerWidth) {
-        this.spritePos = spritePos;
-        this.canvas = canvas;
-        this.canvasCtx = canvas.getContext('2d');
-        this.xPos = containerWidth - 50;
-        this.yPos = 30;
-        this.currentPhase = 0;
-        this.opacity = 0;
-        this.containerWidth = containerWidth;
-        this.stars = [];
-        this.drawStars = false;
-        this.placeStars();
-    };
+    // function NightMode(canvas, spritePos, containerWidth) {
+    //     this.spritePos = spritePos;
+    //     this.canvas = canvas;
+    //     this.canvasCtx = canvas.getContext('2d');
+    //     this.xPos = containerWidth - 50;
+    //     this.yPos = 30;
+    //     this.currentPhase = 0;
+    //     this.opacity = 0;
+    //     this.containerWidth = containerWidth;
+    //     this.stars = [];
+    //     this.drawStars = false;
+    //     this.placeStars();
+    // };
 
-    /**
-     * @enum {number}
-     */
-    NightMode.config = {
-        FADE_SPEED: 0.035,
-        HEIGHT: 40,
-        MOON_SPEED: 0.25,
-        NUM_STARS: 2,
-        STAR_SIZE: 9,
-        STAR_SPEED: 0.3,
-        STAR_MAX_Y: 70,
-        WIDTH: 20
-    };
+    // /**
+    //  * @enum {number}
+    //  */
+    // NightMode.config = {
+    //     FADE_SPEED: 0.035,
+    //     HEIGHT: 40,
+    //     MOON_SPEED: 0.25,
+    //     NUM_STARS: 2,
+    //     STAR_SIZE: 9,
+    //     STAR_SPEED: 0.3,
+    //     STAR_MAX_Y: 70,
+    //     WIDTH: 20
+    // };
 
-    NightMode.phases = [140, 120, 100, 60, 40, 20, 0];
+    // NightMode.phases = [140, 120, 100, 60, 40, 20, 0];
 
-    NightMode.prototype = {
-        /**
-         * Update moving moon, changing phases.
-         * @param {boolean} activated Whether night mode is activated.
-         * @param {number} delta
-         */
-        update: function (activated, delta) {
-            // Moon phase.
-            if (activated && this.opacity == 0) {
-                this.currentPhase++;
+    // NightMode.prototype = {
+    //     /**
+    //      * Update moving moon, changing phases.
+    //      * @param {boolean} activated Whether night mode is activated.
+    //      * @param {number} delta
+    //      */
+    //     update: function (activated, delta) {
+    //         // Moon phase.
+    //         if (activated && this.opacity == 0) {
+    //             this.currentPhase++;
 
-                if (this.currentPhase >= NightMode.phases.length) {
-                    this.currentPhase = 0;
-                }
-            }
+    //             if (this.currentPhase >= NightMode.phases.length) {
+    //                 this.currentPhase = 0;
+    //             }
+    //         }
 
-            // Fade in / out.
-            if (activated && (this.opacity < 1 || this.opacity == 0)) {
-                this.opacity += NightMode.config.FADE_SPEED;
-            } else if (this.opacity > 0) {
-                this.opacity -= NightMode.config.FADE_SPEED;
-            }
+    //         // Fade in / out.
+    //         if (activated && (this.opacity < 1 || this.opacity == 0)) {
+    //             this.opacity += NightMode.config.FADE_SPEED;
+    //         } else if (this.opacity > 0) {
+    //             this.opacity -= NightMode.config.FADE_SPEED;
+    //         }
 
-            // Set moon positioning.
-            if (this.opacity > 0) {
-                this.xPos = this.updateXPos(this.xPos, NightMode.config.MOON_SPEED);
+    //         // Set moon positioning.
+    //         if (this.opacity > 0) {
+    //             this.xPos = this.updateXPos(this.xPos, NightMode.config.MOON_SPEED);
 
-                // Update stars.
-                if (this.drawStars) {
-                    for (var i = 0; i < NightMode.config.NUM_STARS; i++) {
-                        this.stars[i].x = this.updateXPos(this.stars[i].x,
-                            NightMode.config.STAR_SPEED);
-                    }
-                }
-                this.draw();
-            } else {
-                this.opacity = 0;
-                this.placeStars();
-            }
-            this.drawStars = true;
-        },
+    //             // Update stars.
+    //             if (this.drawStars) {
+    //                 for (var i = 0; i < NightMode.config.NUM_STARS; i++) {
+    //                     this.stars[i].x = this.updateXPos(this.stars[i].x,
+    //                         NightMode.config.STAR_SPEED);
+    //                 }
+    //             }
+    //             this.draw();
+    //         } else {
+    //             this.opacity = 0;
+    //             this.placeStars();
+    //         }
+    //         this.drawStars = true;
+    //     },
 
-        updateXPos: function (currentPos, speed) {
-            if (currentPos < -NightMode.config.WIDTH) {
-                currentPos = this.containerWidth;
-            } else {
-                currentPos -= speed;
-            }
-            return currentPos;
-        },
+    //     updateXPos: function (currentPos, speed) {
+    //         if (currentPos < -NightMode.config.WIDTH) {
+    //             currentPos = this.containerWidth;
+    //         } else {
+    //             currentPos -= speed;
+    //         }
+    //         return currentPos;
+    //     },
 
-        draw: function () {
-            var moonSourceWidth = this.currentPhase == 3 ? NightMode.config.WIDTH * 2 :
-                NightMode.config.WIDTH;
-            var moonSourceHeight = NightMode.config.HEIGHT;
-            var moonSourceX = this.spritePos.x + NightMode.phases[this.currentPhase];
-            var moonOutputWidth = moonSourceWidth;
-            var starSize = NightMode.config.STAR_SIZE;
-            var starSourceX = Runner.spriteDefinition.LDPI.STAR.x;
+    //     draw: function () {
+    //         var moonSourceWidth = this.currentPhase == 3 ? NightMode.config.WIDTH * 2 :
+    //             NightMode.config.WIDTH;
+    //         var moonSourceHeight = NightMode.config.HEIGHT;
+    //         var moonSourceX = this.spritePos.x + NightMode.phases[this.currentPhase];
+    //         var moonOutputWidth = moonSourceWidth;
+    //         var starSize = NightMode.config.STAR_SIZE;
+    //         var starSourceX = Runner.spriteDefinition.LDPI.STAR.x;
 
-            if (IS_HIDPI) {
-                moonSourceWidth *= 2;
-                moonSourceHeight *= 2;
-                moonSourceX = this.spritePos.x +
-                    (NightMode.phases[this.currentPhase] * 2);
-                starSize *= 2;
-                starSourceX = Runner.spriteDefinition.HDPI.STAR.x;
-            }
+    //         if (IS_HIDPI) {
+    //             moonSourceWidth *= 2;
+    //             moonSourceHeight *= 2;
+    //             moonSourceX = this.spritePos.x +
+    //                 (NightMode.phases[this.currentPhase] * 2);
+    //             starSize *= 2;
+    //             starSourceX = Runner.spriteDefinition.HDPI.STAR.x;
+    //         }
 
-            this.canvasCtx.save();
-            this.canvasCtx.globalAlpha = this.opacity;
+    //         this.canvasCtx.save();
+    //         this.canvasCtx.globalAlpha = this.opacity;
 
-            // Stars.
-            if (this.drawStars) {
-                for (var i = 0; i < NightMode.config.NUM_STARS; i++) {
-                    this.canvasCtx.drawImage(Runner.imageSprite,
-                        starSourceX, this.stars[i].sourceY, starSize, starSize,
-                        Math.round(this.stars[i].x), this.stars[i].y,
-                        NightMode.config.STAR_SIZE, NightMode.config.STAR_SIZE);
-                }
-            }
+    //         // Stars.
+    //         if (this.drawStars) {
+    //             for (var i = 0; i < NightMode.config.NUM_STARS; i++) {
+    //                 this.canvasCtx.drawImage(Runner.imageSprite,
+    //                     starSourceX, this.stars[i].sourceY, starSize, starSize,
+    //                     Math.round(this.stars[i].x), this.stars[i].y,
+    //                     NightMode.config.STAR_SIZE, NightMode.config.STAR_SIZE);
+    //             }
+    //         }
 
-            // Moon.
-            this.canvasCtx.drawImage(Runner.imageSprite, moonSourceX,
-                this.spritePos.y, moonSourceWidth, moonSourceHeight,
-                Math.round(this.xPos), this.yPos,
-                moonOutputWidth, NightMode.config.HEIGHT);
+    //         // Moon.
+    //         this.canvasCtx.drawImage(Runner.imageSprite, moonSourceX,
+    //             this.spritePos.y, moonSourceWidth, moonSourceHeight,
+    //             Math.round(this.xPos), this.yPos,
+    //             moonOutputWidth, NightMode.config.HEIGHT);
 
-            this.canvasCtx.globalAlpha = 1;
-            this.canvasCtx.restore();
-        },
+    //         this.canvasCtx.globalAlpha = 1;
+    //         this.canvasCtx.restore();
+    //     },
 
-        // Do star placement.
-        placeStars: function () {
-            var segmentSize = Math.round(this.containerWidth /
-                NightMode.config.NUM_STARS);
+    //     // Do star placement.
+    //     placeStars: function () {
+    //         var segmentSize = Math.round(this.containerWidth /
+    //             NightMode.config.NUM_STARS);
 
-            for (var i = 0; i < NightMode.config.NUM_STARS; i++) {
-                this.stars[i] = {};
-                this.stars[i].x = getRandomNum(segmentSize * i, segmentSize * (i + 1));
-                this.stars[i].y = getRandomNum(0, NightMode.config.STAR_MAX_Y);
+    //         for (var i = 0; i < NightMode.config.NUM_STARS; i++) {
+    //             this.stars[i] = {};
+    //             this.stars[i].x = getRandomNum(segmentSize * i, segmentSize * (i + 1));
+    //             this.stars[i].y = getRandomNum(0, NightMode.config.STAR_MAX_Y);
 
-                if (IS_HIDPI) {
-                    this.stars[i].sourceY = Runner.spriteDefinition.HDPI.STAR.y +
-                        NightMode.config.STAR_SIZE * 2 * i;
-                } else {
-                    this.stars[i].sourceY = Runner.spriteDefinition.LDPI.STAR.y +
-                        NightMode.config.STAR_SIZE * i;
-                }
-            }
-        },
+    //             if (IS_HIDPI) {
+    //                 this.stars[i].sourceY = Runner.spriteDefinition.HDPI.STAR.y +
+    //                     NightMode.config.STAR_SIZE * 2 * i;
+    //             } else {
+    //                 this.stars[i].sourceY = Runner.spriteDefinition.LDPI.STAR.y +
+    //                     NightMode.config.STAR_SIZE * i;
+    //             }
+    //         }
+    //     },
 
-        reset: function () {
-            this.currentPhase = 0;
-            this.opacity = 0;
-            this.update(false);
-        }
+    //     reset: function () {
+    //         this.currentPhase = 0;
+    //         this.opacity = 0;
+    //         this.update(false);
+    //     }
 
-    };
+    // };
 
 
     //******************************************************************************
@@ -2541,8 +2549,9 @@
         this.sourceDimensions = {};
         this.dimensions = HorizonLine.dimensions;
         //在雪碧图中坐标为2和602处分别为不同的地形
-        this.sourceXPos = [this.spritePos.x +
-            this.dimensions.WIDTH, this.spritePos.x];
+        // this.sourceXPos = [this.spritePos.x +
+        //     this.dimensions.WIDTH, this.spritePos.x];
+        this.sourceXPos = this.spritePos.x;
 
         this.xPos = [];  //地面在画布中的x坐标
         this.yPos = 0;   //地面在画布中的y坐标
@@ -2559,7 +2568,7 @@
      * @enum {number}
      */
     HorizonLine.dimensions = {
-        WIDTH: 460,
+        WIDTH: 600,
         HEIGHT: 100,
         YPOS: 268
     };
@@ -2585,7 +2594,7 @@
                 this.dimensions[dimension] = HorizonLine.dimensions[dimension];
             }
 
-            this.xPos = [0, HorizonLine.dimensions.WIDTH];
+            this.xPos = 0;
             this.yPos = HorizonLine.dimensions.YPOS;
         },
 
@@ -2593,25 +2602,25 @@
          * Return the crop x position of a type.
          * 随机地形
          */
-        getRandomType: function () {
-            return Math.random() > this.bumpThreshold ? this.dimensions.WIDTH : 0;
-        },
+        // getRandomType: function () {
+        //     return Math.random() > this.bumpThreshold ? this.dimensions.WIDTH : 0;
+        // },
 
         /**
          * Draw the horizon line.
          */
-        draw: function () {
-            this.canvasCtx.drawImage(Runner.imageSprite, this.sourceXPos[0],
+        draw: function () {           
+            this.canvasCtx.drawImage(Runner.imageSprite, this.sourceXPos,
                 this.spritePos.y,
                 this.sourceDimensions.WIDTH, this.sourceDimensions.HEIGHT,
-                this.xPos[0], this.yPos,
+                this.xPos, this.yPos,
                 this.dimensions.WIDTH, this.dimensions.HEIGHT);
 
-            this.canvasCtx.drawImage(Runner.imageSprite, this.sourceXPos[1],
-                this.spritePos.y,
-                this.sourceDimensions.WIDTH, this.sourceDimensions.HEIGHT,
-                this.xPos[1], this.yPos,
-                this.dimensions.WIDTH, this.dimensions.HEIGHT);
+            // this.canvasCtx.drawImage(Runner.imageSprite, this.sourceXPos[1],
+            //     this.spritePos.y,
+            //     this.sourceDimensions.WIDTH, this.sourceDimensions.HEIGHT,
+            //     this.xPos[1], this.yPos,
+            //     this.dimensions.WIDTH, this.dimensions.HEIGHT);
         },
 
         /**
@@ -2619,19 +2628,19 @@
          * @param {number} pos Line position.
          * @param {number} increment
          */
-        updateXPos: function (pos, increment) {
-            var line1 = pos;
-            var line2 = pos == 0 ? 1 : 0;
+        // updateXPos: function (pos, increment) {
+        //     var line1 = pos;
+        //     var line2 = pos == 0 ? 1 : 0;
 
-            this.xPos[line1] -= increment;
-            this.xPos[line2] = this.xPos[line1] + this.dimensions.WIDTH;
+        //     this.xPos[line1] -= increment;
+        //     this.xPos[line2] = this.xPos[line1] + this.dimensions.WIDTH;
 
-            if (this.xPos[line1] <= -this.dimensions.WIDTH) {
-                this.xPos[line1] += this.dimensions.WIDTH * 2;
-                this.xPos[line2] = this.xPos[line1] - this.dimensions.WIDTH;
-                this.sourceXPos[line1] = this.getRandomType() + this.spritePos.x;
-            }
-        },
+        //     if (this.xPos[line1] <= -this.dimensions.WIDTH) {
+        //         this.xPos[line1] += this.dimensions.WIDTH * 2;
+        //         this.xPos[line2] = this.xPos[line1] - this.dimensions.WIDTH;
+        //         this.sourceXPos[line1] = this.getRandomType() + this.spritePos.x;
+        //     }
+        // },
 
         /**
          * Update the horizon line.
@@ -2641,11 +2650,11 @@
         update: function (deltaTime, speed) {
             var increment = Math.floor(speed * (FPS / 1000) * deltaTime);
 
-            if (this.xPos[0] <= 0) {
-                this.updateXPos(0, increment);
-            } else {
-                this.updateXPos(1, increment);
-            }
+            // if (this.xPos[0] <= 0) {
+            //     this.updateXPos(0, increment);
+            // } else {
+            //     this.updateXPos(1, increment);
+            // }
             this.draw();
         },
 
@@ -2680,7 +2689,7 @@
         this.horizonOffsets = [0, 0];
         this.cloudFrequency = this.config.CLOUD_FREQUENCY;
         this.spritePos = spritePos;
-        this.nightMode = null;
+        // this.nightMode = null;
 
         // Cloud
         this.clouds = [];
@@ -2712,8 +2721,8 @@
         init: function () {
             this.addCloud();
             this.horizonLine = new HorizonLine(this.canvas, this.spritePos.HORIZON);
-            this.nightMode = new NightMode(this.canvas, this.spritePos.MOON,
-                this.dimensions.WIDTH);
+            // this.nightMode = new NightMode(this.canvas, this.spritePos.MOON,
+            //     this.dimensions.WIDTH);
         },
 
         /**
@@ -2727,7 +2736,7 @@
         update: function (deltaTime, currentSpeed, updateObstacles, showNightMode) {
             this.runningTime += deltaTime;
             this.horizonLine.update(deltaTime, currentSpeed);
-            this.nightMode.update(showNightMode);
+            // this.nightMode.update(showNightMode);
             this.updateClouds(deltaTime, currentSpeed);
 
             if (updateObstacles) {
@@ -2860,7 +2869,7 @@
         reset: function () {
             this.obstacles = [];
             this.horizonLine.reset();
-            this.nightMode.reset();
+            // this.nightMode.reset();
         },
 
         /**
