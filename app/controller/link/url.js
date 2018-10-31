d7e4e4334b7938bae554cdff0ac7efa2018f8cd5
Jose <alonehover@qq.com>
2018-11-01 00:10:07 +0800

    download favicon

--- app/controller/link/url.js
+++ app/controller/link/url.js
@@ -1,11 +1,12 @@
 'use strict';
+const path = require('path');
+const fs = require('fs');
 
 const Controller = require('egg').Controller;
 
 class UrlController extends Controller {
   async index() {
     const { ctx } = this;
-    // const user = await this.app.mysql.query('select * from link_user');
     await ctx.render('link/index', { name: 'url list' });
   }
 
@@ -16,8 +17,35 @@ class UrlController extends Controller {
   }
 
   async add() {
+    const { ctx, app } = this;
+    const { name, description, url } = ctx.request.body;
+    if(!name || !description || !url) {
+      this.ctx.body = {
+        data: '',
+        msg: '缺少必要参数'
+      };
+     
+      return;
+    }
+    const host = url.split('//')[1].split('/')[0];
+    const icoUrl = 'http://' + host + '/favicon.ico';
+    let icoPath = host + '.ico';
+
+    try {
+      await ctx.curl(icoUrl, {
+        writeStream: fs.createWriteStream(path.resolve(process.cwd(), 'app/public/favicons/' + icoPath)),
+      });
+    } catch (error) {
+      icoPath = '';
+    }
+
+    const user = await app.mysql.insert('link_url', { name, description, url, icon: icoPath });
+
     this.ctx.body = {
       name: '添加链接',
+      data: user,
+      icoUrl,
+      icoPath
     };
   }
 
